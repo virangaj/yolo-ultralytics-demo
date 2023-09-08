3c3d56c21398b515be2c70a93382dbe291c53e43
virangaj <virangapasindu4@gmail.com>
2023-09-08 12:01:27 +0530

    for 30 epochs

--- main.py
+++ main.py
@@ -6,5 +6,5 @@ model = YOLO('yolov8m.yaml')
 result = model.train(data='config.yaml', epochs=30)
 
 metrics = model.val()  # evaluate model performance on the validation set
-results = model("F:/AI ML DL Projects/Supports/Yolo object detection/dataset/data/images/val/134.jpg")  # predict on an image
+results = model("/home/cs304/s17391/yolo-demo/yolo-ultralytics-demo/dataset/data/images/val/134.jpg")  # predict on an image
 path = model.export(format="onnx")  # export the model to ONNX format

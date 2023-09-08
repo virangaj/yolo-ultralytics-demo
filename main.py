2682733c00d1007d05583dacdaad3a5e26b3214a
Viranga Jayawardana <virangapasindu4@gmail.com>
2023-09-08 11:30:59 +0530

    change the config

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
 from ultralytics import YOLO
 
 #Load the model
-model = YOLO('yolov8n.yaml')
+model = YOLO('yolov8m.yaml')
 
-result = model.train(data='config.yaml', epochs=2)
+result = model.train(data='config.yaml', epochs=30)
 
 metrics = model.val()  # evaluate model performance on the validation set
 results = model("F:/AI ML DL Projects/Supports/Yolo object detection/dataset/data/images/val/134.jpg")  # predict on an image

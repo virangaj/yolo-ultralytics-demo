57005471ba56864a8b5ca6872755785a58765232
virangaj <virangapasindu4@gmail.com>
2023-09-12 14:33:26 +0530

    train_size15_epoch150

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ from ultralytics import YOLO
 #Load the model
 model = YOLO('yolov8m.yaml')
 
-result = model.train(data='config.yaml', epochs=100)
+result = model.train(data='config.yaml', epochs=150)
 
 metrics = model.val()  # evaluate model performance on the validation set
 results = model("/home/cs304/s17391/yolo-demo/yolo-ultralytics-demo/dataset/data/images/val/134.jpg")  # predict on an image
